e10148f11340aa3d16871c1b1d3c664adc038e40
Alec Vaughn <alecj.vaughn@gmail.com>
2026-06-20 19:58:31 -0500

    chore(inventory): Initialize Quarkus Maven project

---
+++ inventory-service/src/main/java/com/example/inventory/HealthResource.java
@@ -0,0 +1,16 @@
+package com.example.inventory;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/healthz")
+public class HealthResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+}
